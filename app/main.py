8762c2ff68b86183f056c251d0e20db6e47e29e7
Chirag <chirag657krsingh@gmail.com>
2026-03-24 21:03:16 +0530

    Add FastAPI endpoint for chatbot API

--- app/main.py
+++ app/main.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from app.rag.pipeline import ask_question
 
 app = FastAPI()
 
+
+# Request body schema
+class QueryRequest(BaseModel):
+    query: str
+
+
 @app.get("/")
 def home():
-    return {"message": "RAG Knowledge Assistant Running"}
+    return {"message": "RAG Chatbot API is running"}
+
+
+@app.post("/ask")
+def ask(request: QueryRequest):
+
+    query = request.query
+
+    answer = ask_question(query)
+
+    return {
+        "query": query,
+        "answer": answer
+    }
